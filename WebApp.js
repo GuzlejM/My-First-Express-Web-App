1534a672e5d565ecc6c398d508f6382b094d2b1a
GuzlejM <59050286+GuzlejM@users.noreply.github.com>
2020-02-25 11:35:00 +0000

    Add files via upload

---
+++ WebApp.js
@@ -0,0 +1,31 @@
+var express = require("express");
+var app = express();
+
+//Prints - "Hi there, welcome to my assignment!"
+app.get("/", function(req, res) {
+    res.send("Hi there, welcome to my assignment!");
+});
+
+app.get("/:arena", function(req, res) {
+    res.send("Welcome to Arena, mfker!");
+});
+
+app.get("/", function(req, res) {
+    res.send("Hi there!");
+});
+
+app.get("/", function(req, res) {
+    res.send("Hi there!");
+});
+
+//If you try any other not assigned route this prints out
+app.get("*", function(req, res) {
+    res.send("Sorry, page not found...What are you doing with your life?");
+});
+
+
+//tell Express to listen for requests (start server)
+var port = process.env.PORT || 3001;
+app.listen(port, function() {
+    console.log("Server Has Started!");
+})
