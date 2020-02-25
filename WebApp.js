fe7a10bb8520e9a8485cd1d45e6a9abf6bb41a3b
GuzlejM <59050286+GuzlejM@users.noreply.github.com>
2020-02-25 13:33:11 +0000

    Update WebApp.js

--- WebApp.js
+++ WebApp.js
@@ -1,11 +1,14 @@
 var express = require("express");
 var app = express();
 
+//First objective
 //Prints - "Hi there, welcome to my assignment!"
 app.get("/", function(req, res) {
     res.send("Hi there, welcome to my assignment!");
 });
 
+
+//Second objective
 //This is easy as you can see
 app.get("/speak/pig", function(req, res) {
     res.send("The pig says 'Oink'");
@@ -17,6 +20,19 @@ app.get("/speak/cow", function(req, res) {
 
 app.get("/speak/dog", function(req, res) {
     res.send("The dog says 'Woof Woof'");
+});
+
+//Third Objective - 
+app.get("/repeat/3", function(req, res) {
+    res.send('"hello hello hello"');
+});
+
+app.get("/repeat/5", function(req, res) {
+    res.send('"hello hello hello hello hello"');
+});
+
+app.get("/repeat/2", function(req, res) {
+    res.send('"blah blah"');
 });
 
 //If you try any other not assigned route this prints out
